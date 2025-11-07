19ca5d0b9a32271061086a272007ae15ec2d72e0
Mahdar <mahdardavari@gmail.com>
2025-11-07 22:04:16 +0330

    fix Book Event added booking.actions.ts

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -1,39 +1,48 @@
-import Link from "next/link";
-import Image from "next/image";
+'use client';
 
-interface Props {
-    title: string;
-    image: string;
-    slug: string;
-    location: string;
-    date: string;
-    time: string;
-}
+import {useState} from "react";
+import {createBooking} from "@/lib/actions/booking.actions";
+import posthog from "posthog-js";
 
-const EventCard = ({title, image, slug, location, date, time}: Props) => {
+const BookEvent = ({eventId, slug}: { eventId: string, slug: string; }) => {
+    const [email, setEmail] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const {success} = await createBooking({eventId, slug, email});
+
+        if (success) {
+            setSubmitted(true);
+            posthog.capture('event_booked', {eventId, slug, email})
+        } else {
+            console.error('Booking creation failed')
+            posthog.captureException('Booking creation failed')
+        }
+    }
+
     return (
-        <Link href={`/events/${slug}`} id="event-card">
-            <Image src={image} alt={title} width={410} height={300} className="poster"/>
+        <div id="book-event">
+            {submitted ? (
+                <p className="text-sm">Thank you for signing up!</p>
+            ) : (
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="email">Email Address</label>
+                        <input
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            id="email"
+                            placeholder="Enter your email address"
+                        />
+                    </div>
 
-            <div className="flex flex-row gap-2">
-                <Image src="/icons/pin.svg" alt="location" width={14} height={14}/>
-                <p>{location}</p>
-            </div>
-
-            <p className="title">{title}</p>
-
-            <div className="datetime">
-                <div>
-                    <Image src="/icons/calendar.svg" alt="date" width={14} height={14}/>
-                    <p>{date}</p>
-                </div>
-                <div>
-                    <Image src="/icons/clock.svg" alt="time" width={14} height={14}/>
-                    <p>{time}</p>
-                </div>
-            </div>
-        </Link>
+                    <button type="submit" className="button-submit">Submit</button>
+                </form>
+            )}
+        </div>
     )
 }
-
-export default EventCard
+export default BookEvent
